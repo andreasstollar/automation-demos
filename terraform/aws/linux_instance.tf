d84388c7a8621931e85d6faee54fc0e6f2dfcf71
Andreas Stollar <andreas.stollar@gdt.com>
2020-09-17 08:59:23 -0700

    Update to use centos image, which is more usable

--- terraform/aws/linux_instance.tf
+++ terraform/aws/linux_instance.tf
@@ -21,4 +21,32 @@ resource "aws_instance" "ubuntu-20-04" {
 output "ubuntu-20-04-ip" {
   value = "${aws_instance.ubuntu-20-04.public_ip}"
 }
+
+data "aws_ami" "centos_ami" {
+  most_recent = true
+  owners      = ["125523088429"]
+  filter {
+    name   = "name"
+    values = ["CentOS 8.2.*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "centos-8" {
+  ami                    = data.aws_ami.centos_ami.image_id
+  instance_type          = "t2.micro"
+  key_name               = "andreas_gdt_key"
+  subnet_id              = aws_subnet.terraform_demo_Subnet.id
+  vpc_security_group_ids = [aws_security_group.terraform_demo_Security_Group.id]
+  tags = {
+    Name = "terraform-demo-centos"
+  }
+}
+
+output "centos8-ip" {
+  value = "${aws_instance.centos-8.public_ip}"
+}
 # end vpc.tf
