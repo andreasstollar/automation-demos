381a708509588773250c02ea8f8f6470137e897b
Andreas Stollar <andreas.stollar@gdt.com>
2020-11-11 13:29:32 -0800

    add machine type variable, add formula to instantiate instance in existing network

---
+++ terraform/gcp/linux_instance_existing_vpc.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  version = "~> 3.39"
+  project = var.project_name
+  region  = var.region
+  zone    = var.zone
+}
+
+data "google_compute_image" "ubuntu_image" {
+  family  = "ubuntu-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_instance" "ubuntu_2004" {
+  count        = var.node_count
+  name         = "terraform-demo-ubuntu-${count.index + 1}"
+  machine_type = var.machine_type
+  tags         = ["ssh", "http"]
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu_image.self_link
+    }
+  }
+
+  network_interface {
+    network    = "default"
+    access_config {
+    }
+  }
+}
+
+output "ubuntu_2004_ip" {
+  value = google_compute_instance.ubuntu_2004[*].network_interface[0].access_config[0].nat_ip
+}
+
