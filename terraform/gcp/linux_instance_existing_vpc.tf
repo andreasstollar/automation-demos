a12ae54fcbb5a697a6de79d0fac9609e45c82e9c
Andreas Stollar <andreas.stollar@gdt.com>
2021-02-04 09:08:18 -0800

    set index on name when count is set

--- terraform/gcp/linux_instance_existing_vpc.tf
+++ terraform/gcp/linux_instance_existing_vpc.tf
@@ -12,7 +12,9 @@ data "google_compute_image" "ubuntu_image" {
 
 resource "google_compute_instance" "ubuntu_2004" {
   count        = var.node_count
-  name         = "automation-demo"
+  #name         = var.VMName
+  #when count is set, name must have an index, even if count is 1
+  name         = "terraform-demo-ubuntu-${count.index + 1}"
   machine_type = var.machine_type
   tags         = ["ssh", "http", "https", "http-8080", "https-8443"]
   zone         = var.zone
